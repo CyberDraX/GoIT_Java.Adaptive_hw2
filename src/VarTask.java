ea5f5d1220b436e9de0a2cfbd08e44ceca7d8f4e
Vadim Korolov <vadim.v.korolov@gmail.com>
2024-06-25 18:56:48 +0300

    Task 1

---
+++ src/VarTask.java
@@ -0,0 +1,8 @@
+public class VarTask {
+    public static void main(String[] args) {
+        byte happyValue = 15;
+
+        //Example output
+        System.out.println("happyValue = " + happyValue);
+    }
+}
