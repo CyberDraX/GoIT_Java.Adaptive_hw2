a05aa59980f9f69f539fea9767f58ef4cb4df798
Vadim Korolov <vadim.v.korolov@gmail.com>
2024-07-06 00:34:21 +0300

    Task 3

--- src/VarTask.java
+++ src/VarTask.java
@@ -2,8 +2,14 @@ public class VarTask {
     public static void main(String[] args) {
         byte happyValue = 8;
         short cold = -5;
-        
+        int globalAnswer = 42;
+        int randomAnswer;
+        randomAnswer = 140;
+
+
         System.out.println("happyValue = " + happyValue);
         System.out.println("cold = " + cold);
+        System.out.println("globalAnswer = " + globalAnswer);
+        System.out.println("randomAnswer = " + randomAnswer);
     }
 }
