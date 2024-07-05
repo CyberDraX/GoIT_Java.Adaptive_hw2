db058d47c94584a41b2c7fbae8382366c368dd01
Vadim Korolov <vadim.v.korolov@gmail.com>
2024-07-05 21:01:18 +0300

    Task 2

--- src/VarTask.java
+++ src/VarTask.java
@@ -2,8 +2,7 @@ public class VarTask {
     public static void main(String[] args) {
         byte happyValue = 8;
         short cold = -5;
-
-        //Example output
+        
         System.out.println("happyValue = " + happyValue);
         System.out.println("cold = " + cold);
     }
