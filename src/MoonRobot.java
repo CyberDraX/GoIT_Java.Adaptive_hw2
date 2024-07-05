3826f9134a6aea089cc36f6eb1cd0abbc1b4521c
Vadim Korolov <vadim.v.korolov@gmail.com>
2024-07-06 02:41:45 +0300

    Task 29

--- src/MoonRobot.java
+++ src/MoonRobot.java
@@ -4,10 +4,17 @@ public class MoonRobot {
         return holeDepth < 34 || batteryPercent > 67;
     }
 
+    public boolean canJumpOverHill(int hillHeight, int batteryPercent){
+        return hillHeight <= 150 && batteryPercent >= 90;
+    }
+
     public static void main(String[] args) {
         MoonRobot robot = new MoonRobot();
 
         //Should be false
         System.out.println("canOvercomeHole(50, 60) = " + robot.canOvercomeHole(50, 60));
+
+        //Should be true
+        System.out.println("canJumpOverHill(100, 90) = " + robot.canJumpOverHill(100, 90));
     }
 }
