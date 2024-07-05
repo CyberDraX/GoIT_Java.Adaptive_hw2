a60feb6118b46ea6697beefa04160fe1e817c59a
Vadim Korolov <vadim.v.korolov@gmail.com>
2024-07-06 01:07:36 +0300

    Task 11

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -4,10 +4,17 @@ public class MathUtils {
         return a + b;
     }
 
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
         MathUtils math = new MathUtils();
 
         //Should be 5
         System.out.println("math.sum(3, 2) = " + math.sum(3, 2));
+
+        //Should be 1
+        System.out.println("math.sub(3, 2) = " + math.sub(3, 2));
     }
 }
