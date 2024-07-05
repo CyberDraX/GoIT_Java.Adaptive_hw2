f125ecb25753da29a89d117b503ff503ccc2d6da
Vadim Korolov <vadim.v.korolov@gmail.com>
2024-07-06 00:47:24 +0300

    Task 7

--- src/VarTask.java
+++ src/VarTask.java
@@ -8,6 +8,9 @@ public class VarTask {
         long longerThanLife = 153474353453278L;
         float tooBigToFail = 1111.1f;
         double muchBiggerThanTooBig = -103215677.5d;
+        char englishA = 'A';
+        char anyRuLetter = 'п';
+        char codeName70 = 'F';
 
 
         System.out.println("happyValue = " + happyValue);
@@ -17,5 +20,8 @@ public class VarTask {
         System.out.println("longerThanLife = " + longerThanLife);
         System.out.println("tooBigToFail = " + tooBigToFail);
         System.out.println("muchBiggerThanTooBig = " + muchBiggerThanTooBig);
+        System.out.println("englishA = " + englishA);
+        System.out.println("anyRuLetter = " + anyRuLetter);
+        System.out.println("codeName70 = " + codeName70);
     }
 }
