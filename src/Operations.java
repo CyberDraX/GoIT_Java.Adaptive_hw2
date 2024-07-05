4e9b05ca1d23005ba5beb769c098d934e20b589f
Vadim Korolov <vadim.v.korolov@gmail.com>
2024-07-06 01:32:16 +0300

    Task 16

--- src/Operations.java
+++ src/Operations.java
@@ -3,6 +3,7 @@ public class Operations {
     public static void main(String[] args) {
         short shouldBeIncreased = 5;
               shouldBeIncreased++;
+              shouldBeIncreased--;
 
         System.out.println(shouldBeIncreased);
     }
