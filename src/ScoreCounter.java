bd6fb096969173e3fc6812d737755bdff6a0760c
Vadim Korolov <vadim.v.korolov@gmail.com>
2024-07-06 01:49:52 +0300

    Task 19

--- src/ScoreCounter.java
+++ src/ScoreCounter.java
@@ -5,8 +5,18 @@ public class ScoreCounter {
         return currentScore;
     }
 
+    public int twiceScore(int currentScore) {
+        currentScore *= 2;
+        return currentScore;
+    }
+
     public static void main(String[] args) {
         ScoreCounter counter = new ScoreCounter();
+
+        //Should be 15
         System.out.println("addScore(10, 5) = " + counter.addScore(10, 5));
+
+        //Should be 14
+        System.out.println("twiceScore(7) = " + counter.twiceScore(7));
     }
 }
