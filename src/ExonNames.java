58cb7a8af9ae642f857f7af8fb07a39a32004dfd
Vadim Korolov <vadim.v.korolov@gmail.com>
2024-07-06 03:11:43 +0300

    Task 38

--- src/ExonNames.java
+++ src/ExonNames.java
@@ -4,11 +4,19 @@ public class ExonNames {
         return name1.equals(name2);
     }
 
+    public String makeFullName (String firstName, String lastName){
+        return firstName.toLowerCase() + " " + lastName.toUpperCase();
+    }
+
     public static void main(String[] args) {
         ExonNames names = new ExonNames();
 
         //Should be true
         boolean namesEqual = names.areNamesEqual("nm", "nm");
         System.out.println("names.areNamesEqual(\"nm\", \"nm\") = " + namesEqual);
+
+        //Should be "exor BIGO"
+        String fullName = names.makeFullName("exor", "bigo");
+        System.out.println("names.makeFullName(\"exor\", \"bigo\") = " + fullName);
     }
 }
