e8a7e1bdd2d9e49beb040882a5e52ad491f510db
Vadim Korolov <vadim.v.korolov@gmail.com>
2024-07-06 03:02:08 +0300

    Task 33

---
+++ src/MarsEarthHelper.java
@@ -0,0 +1,13 @@
+public class MarsEarthHelper {
+
+    public int convertAppleCount(float count){
+        return (int) count;
+    }
+
+    public static void main(String[] args) {
+        MarsEarthHelper helper = new MarsEarthHelper();
+
+        //Should be 10
+        System.out.println("convertAppleCount(10f) = " + helper.convertAppleCount(10f));
+    }
+}
